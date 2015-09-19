03d2a7c028083ff800bc7706e302c779ee76965a
Charuwit Nodthaisong <tui2tone@Charuwits-MacBook-Air.local>
2015-09-20 03:42:13 +0700

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# checkbox-io
-CSS3 Style Checkbox
+# Checkbox3
+### CSS3 Style for Checkbox and Radio Button
+
+### Demo
+
+### Usage
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    minify = require("gulp-minify-css"),
+    rename = require('gulp-rename');
+
+gulp.task("compile", function() {
+  gulp.src('checkbox3.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist'))
+    .pipe(minify({compatibility: 'ie8'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist'));
+
+  return gulp.src(['./dist/checkbox3.min.css',]).pipe(gulp.dest('./public/css/'))
+});
+
+gulp.task("lib", function() {
+  gulp.src(['./dist/checkbox3.min.css',]).pipe(gulp.dest('./public/css/'))
+  gulp.src(['./bower_components/fontawesome/css/font-awesome.min.css',]).pipe(gulp.dest('./public/css/'))
+  gulp.src(['./bower_components/fontawesome/fonts/*']).pipe(gulp.dest('./public/fonts'))
+  return gulp.src(['./bower_components/bootstrap/dist/css/bootstrap.min.css',]).pipe(gulp.dest('./public/css/'))
+});
+
+gulp.task("watch", function() {
+  gulp.watch("checkbox3.sass", ['compile'])
+});
